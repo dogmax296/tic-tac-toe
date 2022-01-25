ed662c1f5423aedaf540bc174fa54fd284cbf5ad
dogmax <ivanov1maxim@gmail.com>
2022-01-25 19:36:07 +0200

    Implement the GameFactory logic

--- src/main/java/tictactoe/component/GameFactory.java
+++ src/main/java/tictactoe/component/GameFactory.java
@@ -4,8 +4,10 @@ import tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
 import tictactoe.model.Player;
 import tictactoe.model.PlayerType;
 
-import static tictactoe.model.Sign.O;
-import static tictactoe.model.Sign.X;
+import java.util.Locale;
+
+import static tictactoe.model.PlayerType.*;
+import static tictactoe.model.Sign.*;
 
 /**
  * @author dogmax296
@@ -13,23 +15,64 @@ import static tictactoe.model.Sign.X;
  */
 public class GameFactory {
 
-    private final PlayerType player1Type = PlayerType.USER;
-    private final PlayerType player2Type = PlayerType.COMPUTER;
+    private PlayerType player1Type;
+    private PlayerType player2Type;
 
     public GameFactory(final String[] args) {
-        // TODO
+        PlayerType player1Type = null;
+        PlayerType player2Type = null;
+
+        for (final String arg : args) {
+            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
+                if (player1Type == null) {
+                    player1Type = PlayerType.valueOf(arg.toUpperCase());
+
+                } else if (player2Type == null) {
+                    player2Type = PlayerType.valueOf(arg.toUpperCase());
+                } else {
+                    System.err.println("Unsupported command line argument: '" + arg + "'");
+                }
+            } else {
+                System.err.println("Unsupported command line argument: '" + arg + "'");
+            }
+        }
+
+        if (player1Type == null) {
+            this.player1Type = USER;
+            this.player2Type = COMPUTER;
+        } else if (player2Type == null) {
+            this.player1Type = USER;
+            this.player2Type = player1Type;
+        } else {
+            this.player1Type = player1Type;
+            this.player2Type = player2Type;
+        }
     }
 
     public Game create() {
         final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
+        Player player1 = null;
+        if (this.player1Type == USER) {
+            player1 = new Player(X, new UserMove(cellNumberConverter));
+        } else {
+            player1 = new Player(X, new ComputerMove());
+        }
+
+        Player player2 = null;
+        if (this.player2Type == USER) {
+            player2 = new Player(O, new UserMove(cellNumberConverter));
+        } else {
+            player2 = new Player(O, new ComputerMove());
+        }
+
+        final boolean canSecondPlayerMakeFirstMove = this.player1Type != this.player2Type;
         return new Game(
                 new DataPrinter(cellNumberConverter),
-                // FIXME
-                new Player(X, new UserMove(cellNumberConverter)),
-                new Player(O, new ComputerMove()),
+                player1,
+                player2,
                 new WinnerVerifier(),
                 new CellVerifier(),
-                true
+                canSecondPlayerMakeFirstMove
         );
     }
 }
